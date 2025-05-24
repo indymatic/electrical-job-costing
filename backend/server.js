3f8a55e45abac036674b6cb21467ca0a7dd10eff
Jason <indymatic@gmail.com>
2025-05-24 11:49:19 -0400

    Added friends to my MongoDB toy box

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,46 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 const app = express();
+
+// Add CORS helper to let the playhouse talk to the magic door
+app.use(cors());
 
 // This helper lets us understand messages from the app
 app.use(express.json());
 
 // This is the special line that talks to the toy box (MongoDB)
-// We’ll change the secret code in a minute!
-mongoose.connect('mongodb+srv://admin:admin@cluster0.mongodb.net/electrical-job-costing?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://indymatic:Charly7231!@cluster0.qf37zwn.mongodb.net/electrical-job-costing?retryWrites=true&w=majority')
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));// This is a little test to see if our magic door works
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// This is a blueprint for a friend in the MongoDB toy box
+const friendSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+const Friend = mongoose.model('Friend', friendSchema);
+
+// This is a little test to see if our magic door works
 app.get('/api', (req, res) => {
   res.send('Backend is running!');
+});
+
+// This is a trick to let friends log in by checking the toy box
+app.post('/api/login', async (req, res) => {
+  const { username, password } = req.body;
+  console.log("Received from playhouse:", { username, password });
+
+  // Look in the toy box for a friend with this username and password
+  const friend = await Friend.findOne({ username: username, password: password });
+  console.log("Found friend in toy box:", friend);
+
+  if (friend) {
+    res.send('Welcome, friend! You can come in! 🎉');
+  } else {
+    res.status(401).send('Sorry, I don’t know you! Try again.');
+  }
 });
 
 // This opens the magic door at a special number (5000)
